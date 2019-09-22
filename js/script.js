4b7c7c1132dcbad30cd9ef39f7bcfbbe8400b1d5
audombek <audreyombek@gmail.com>
2019-09-22 20:47:38 +0300

    add a function for fetching data from radio button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-var femaleNames =["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"]
-var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-
-function calcDayOfTheWeek(form){
-var day =parseInt(document.getElementById("Date").value)
-if((day <=0 ) || (day >31)){
-    alert("You've entered an invalid date")
+function getUserDetails(){
+    var date = parseInt(document.getElementById("Date").value);
+    var month =parseInt(document.getElementById("Month").value);
+    var year = parseInt(document.getElementById("Year").value);
+    var gender = radioInfo();
+    alert(gender);
 }
-var month = parseInt(document.getElementById("Month").value)
-if((month <=0) || (month>31)){
-    alert("You've entered an invalid month")
-}
-var year = parseInt(document.getElementById("Year").value)
-if(year<1900){
-    alert("Begins from 1900")
-}
-}
-function getAkanName(){
-    var 
-
-}
+function radioInfo(){
+    var radio = document.getElementsByName('gender');
+    for (var i = 0;  i<radio.length; i++)
+    {
+    if (radio[i].checked)
+    {
+     gender = radio[i].value;
+     return gender;
+    }
+    }
+    }
+    
