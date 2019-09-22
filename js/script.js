410eb5eb1040b122067f66667f2e920b2808aea4
audombek <audreyombek@gmail.com>
2019-09-22 16:04:19 +0300

    added a function to fetch data from forms

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames =["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"]
 var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
-function calcDayOfWeek(){
-    var date = parseInt(document.getElementById("Date").value);
-    if((date<=0)|| (date>31)){
-        alert("You've entered an invalid date");
-    }
-    var month = parseInt(document.getElementById("Month").value);
-    if((month<=0) || (month>12)){
-        alert("You've entered an invalid month")
-    }
-    var year = parseInt(document.getElementById("Year").value);
+function calcDayOfTheWeek(form){
+var day =parseInt(document.getElementById("Date").value)
+if((day <=0 ) || (day >31)){
+    alert("You've entered an invalid date")
+}
+var month = parseInt(document.getElementById("Month").value)
+if((month <=0) || (month>31)){
+    alert("You've entered an invalid month")
+}
+var year = parseInt(document.getElementById("Year").value)
+if(year<1900){
+    alert("Begins from 1900")
+}
+}
+function getAkanName(){
+    var 
 
 }
