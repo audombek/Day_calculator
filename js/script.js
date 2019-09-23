01a622a6765a853511038a8bb9219826a2f56b92
audombek <audreyombek@gmail.com>
2019-09-23 08:30:02 +0300

    changed js functions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-function getUserDetails(){
-    var date = parseInt(document.getElementById("Date").value);
-    var month =parseInt(document.getElementById("Month").value);
-    var year = parseInt(document.getElementById("Year").value);
-    var gender = radioInfo();
-    alert(gender);
-}
-function radioInfo(){
-    var radio = document.getElementsByName('gender');
-    for (var i = 0;  i<radio.length; i++)
-    {
-    if (radio[i].checked)
-    {
-     gender = radio[i].value;
-     return gender;
+var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var female =["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"]
+var dateWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+ 
+function getUserDetails() {
+    var date = parseInt(document.getElementById("date").value);
+    var month =parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+    var gender = getGender();
+    
+    var dayWeek = new Date(year + "/" + month + "/" + date);
+    var d = dayWeek.getDay();
+    var name;
+    if (gender ==="male"){
+    name = male[d];
+        }
+    else{
+    name = female[d];
+        }
+    alert("You were born on "+ dateWeek[d] + " and your Akan name is "+ name);
     }
+function getGender(){
+    var gender = document.getElementsByName('gender');
+    for (i = 0;  i<gender.length; i++){
+    if (gender[i].checked){
+     return (gender[i].value)
+                                                                       
+        }
     }
-    }
+ }
     
